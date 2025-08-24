980b1059529f1b465ef10bb3448306cdd9e0a69a
RohitWazarkar <rohitwazarkar18@gmail.com.com>
2025-08-25 00:59:55 +0530

    in footer added developers name !

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -15,7 +15,7 @@ export default function Layout({ children }) {
 
       {/* Fixed Footer */}
       <footer className="fixed bottom-0 left-0 right-0 bg-blue-600 text-white text-center py-3 z-50">
-        © 2025 StayFeast. All rights reserved.
+        © 2025 StayFeast. All rights reserved -- Created By Mr.Rohit Wazarkar . 
       </footer>
     </div>
   );
