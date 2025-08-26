bce5521bc5b1fb7c5518a9892ecd5d86de69ac1d
RohitWazarkar <rohitwazarkar18@gmail.com.com>
2025-08-26 14:21:28 +0530

    api added hard coded ! resend api

--- src/app/api/feedback/route.js
+++ src/app/api/feedback/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend("re_8iadzmfB_6odohe4imZ2efxjQCToohijp");
 
 export async function POST(req) {
   try {
